c5867be9c09a88d59604c622833718f32c855953
Jinkogule <lucaspimenta@gmail.com>
2022-03-24 11:27:14 -0300

    comentando e organizando codigo dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -55,13 +55,13 @@
                             <?php
                             $amanha = date("Y-m-d", time() + 86400);
 
-                            /*pendente*/
+                            /*Alerta caso refeição esteja pendente e passível de confirmação*/
                             if ($event->status_confirmacao == 'N' && $amanha >= $event->data){
                             ?>
                                 <img src="/images/pendente.png" class="img-fluid" alt="Responsive image" data-toggle="modal" data-target="#confirmacao-notificacao{{$event->id}}" style="position: absolute; width: 20px; height: auto; right: 10px; top: 10px;">
                             <?php
                             }
-                            /*confirmada*/
+                            /*Sinal de confirmada caso refeição esteja confirmada*/
                             elseif ($event->status_confirmacao == 'C'){
                             ?>
                                 <img src="/images/confirmado.png" class="img-fluid" alt="Responsive image" style="position: absolute; width: 20px; height: auto; right: 10px; top: 10px;">
@@ -75,43 +75,42 @@
                             <?php
                             if (2 == 2){
                             ?>
-                            <div class="container capa-cardapio" style="background-image: url('/images/peixe.jpg');">
-                                <div class="cardapio">
-                                    Cardápio: {{$event->cardapio}}
+                                <div class="container capa-cardapio" style="background-image: url('/images/peixe.jpg');">
+                                    <div class="cardapio">
+                                        Cardápio: {{$event->cardapio}}
+                                    </div>
                                 </div>
-                            </div>
-                            <br>
+                                <br>
                             <?php
                             }
                             ?>
                             <?php
-                            /*pendente*/
+                            /*Botões de confirmação e cancelamento caso refeição não esteja confirmada*/
                             if ($event->status_confirmacao == 'N'){
                             ?>
-                            <div class="container botoes-cc" style="margin: 0 auto;">
-                                <div class="row">
-                                    <!--Botão de confirmação disponível caso o dia atual seja 1 anterior à ocorrência da refeição-->
-                                    <?php
-                                    
-                                    if ($amanha >= $event->data){
-                                    ?>
-                                        <div class="d-grid mx-auto mb-3">
-                                            <button type="submit" class="btn btn-sm btn-confirmar" data-toggle="modal" data-target="#confirmacao{{$event->id}}">Confirmar</button>
-                                        </div>
-                                    <?php
-                                    }
-                                    ?>
+                                <div class="container botoes-cc" style="margin: 0 auto;">
+                                    <div class="row">
+                                        <?php
+                                        /*Botão de confirmação disponível caso o dia atual seja 1 anterior à ocorrência da refeição*/
+                                        if ($amanha >= $event->data){
+                                        ?>
+                                            <div class="d-grid mx-auto mb-3">
+                                                <button type="submit" class="btn btn-sm btn-confirmar" data-toggle="modal" data-target="#confirmacao{{$event->id}}">Confirmar</button>
+                                            </div>
+                                        <?php
+                                        }
+                                        ?>
 
-                                    <!--Form cancelamento de refeição-->
-                                    <form id="cancelar_refeicao" action="{{ route('cancelarRefeicao') }}" method="POST">
-                                        @csrf          
-                                        <input type="hidden" id="id_refeicao" name="id_refeicao" value="{{$event->id}}">                
-                                        <div class="d-grid mx-auto mb-2">
-                                            <button type="submit" class="btn btn-sm btn-cancelar">Cancelar</button>
-                                        </div>
-                                    </form> 
+                                        <!--Form cancelamento de refeição-->
+                                        <form id="cancelar_refeicao" action="{{ route('cancelarRefeicao') }}" method="POST">
+                                            @csrf          
+                                            <input type="hidden" id="id_refeicao" name="id_refeicao" value="{{$event->id}}">                
+                                            <div class="d-grid mx-auto mb-2">
+                                                <button type="submit" class="btn btn-sm btn-cancelar">Cancelar</button>
+                                            </div>
+                                        </form> 
+                                    </div>
                                 </div>
-                            </div>
                             <?php
                             }
                             ?>
