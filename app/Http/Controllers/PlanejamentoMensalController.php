f16453c019afc739d06fe05aafd33e67b7e96288
Jinkogule <lucaspimenta@gmail.com>
2022-05-25 00:39:55 -0300

    restringindo select de dias uteis

--- app/Http/Controllers/PlanejamentoMensalController.php
+++ app/Http/Controllers/PlanejamentoMensalController.php
@@ -12,7 +12,7 @@ class PlanejamentoMensalController extends Controller
     public function planejamentomensal(){
         $unidade_bandejao = Auth::user()->unidade_bandejao;
         //$refeicoes = DB::table('refeicaos')->select('*')->where('id_usuario', '=', $id_usuario)->paginate(10);
-        $calendario_dias = DB::table('calendario')->select('*')->where('data', '!=', NULL)->orderBy('data')->paginate(30);
+        $calendario_dias = DB::table('calendario')->select('*')->where('data', '!=', NULL)->orWhere('dia_da_semana', '!=', 'Sábado')->orWhere('dia_da_semana', '!=', 'Domingo')->orderBy('data')->paginate(30);
 
 
         return View::make('layouts.planejamento-mensal')->with('unidade_bandejao', $unidade_bandejao)->with('calendario_dias', $calendario_dias);
