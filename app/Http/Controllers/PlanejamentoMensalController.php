7485c1c7f4a0629984f3551f71df837b903b080e
Jinkogule <lucaspimenta@gmail.com>
2022-05-24 23:48:33 -0300

    correcao

--- app/Http/Controllers/PlanejamentoMensalController.php
+++ app/Http/Controllers/PlanejamentoMensalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PlanejamentoMensalController extends Controller
 {
