37fb58f95fbb6953af35ae60250fed6507590c7b
Jinkogule <lucaspimenta@gmail.com>
2022-05-24 22:48:26 -0300

    add tabela calendario

---
+++ database/migrations/2022_05_25_013242_create_calendario_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalendarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendario', function (Blueprint $table) {
+            $table->id();
+            $table->string('dia_da_semana');
+            $table->string('data_visual');
+            $table->date('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendario');
+    }
+}
