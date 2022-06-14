a1a226e403dd4981dbbb732912e6d7e260b40812
Jinkogule <lucaspimenta@gmail.com>
2022-06-14 11:04:54 -0300

    config mail

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAILGUN_SMTP_SERVER', 'smtp.mailgun.org'),
+            'port' => env('MAILGUN_SMTP_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAILGUN_SMTP_LOGIN'),
+            'password' => env('MAILGUN_SMTP_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
