3c99fcf54e542f522e0609b97b8cc272ce123654
Jinkogule <lucaspimenta@gmail.com>
2022-06-14 20:34:45 -0300

    teste

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new SendEmailJob)->everyFiveMinutes();
+        $schedule->job(new SendEmailJob)->everyMinute();
     }
 
     /**
