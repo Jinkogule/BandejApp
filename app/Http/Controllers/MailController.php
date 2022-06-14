02d0b67f328cc6df62f0bff6548cac86789a30f5
Jinkogule <lucaspimenta@gmail.com>
2022-06-14 17:56:59 -0300

    teste

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -9,8 +9,9 @@ class MailController extends Controller
 {
     public function teste_mail(){
         Mail::send('mail.teste', ['teste' => 'teste'], function($m){
-            $m->from('bandejaoaplicativo@gmail.com');
+            $m->from('BandejApp');
             $m->to('lucaspimenta21@gmail.com');
+            $m->subject('Confirme sua presença no almoço de hoje');
         });
     }
 }
--- resources/views/mail/teste.blade.php
+++ resources/views/mail/teste.blade.php
@@ -1,1 +1,2 @@
-testesstetet
+
+Prezado(a) Lucas, você tem um almoço planejado no Praia Vermelha para amanhã, dia 20/05/2022.
