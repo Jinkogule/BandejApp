1f194ddcfc309c02d72e972f3b51e6ca8c5f276c
Jinkogule <lucaspimenta@gmail.com>
2022-06-16 11:30:31 -0300

    teste

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,11 +10,12 @@ class MailController extends Controller
 {
     public function teste_mail(){
         $usuarios = DB::table('users')->where('id', '!=', '0')->get();
-
-        foreach ($usuarios as $user){
+        $usuarios_email = $usuarios->email;
+        
+        foreach ($usuarios_email as $usuario_email){
             Mail::send('mail.confirmar-presenca', ['confirmar-presenca' => 'confirmar-presenca'], function($m){
                 $m->from('bandejaoaplicativo@gmail.com');
-                $m->to($user->email);
+                $m->to($usuario_email);
                 $m->subject('Confirme sua presença no almoço de hoje');
             });
         }
