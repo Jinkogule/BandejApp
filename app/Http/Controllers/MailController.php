7a1ddd14fb5da4930a7c9cc009ac451ba023495b
Jinkogule <lucaspimenta@gmail.com>
2022-06-16 11:54:11 -0300

    correcao

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class MailController extends Controller
 {
