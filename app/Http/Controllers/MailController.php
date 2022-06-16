89ac089aff49b83a8f25e6a7786c71001bdccf05
Jinkogule <lucaspimenta@gmail.com>
2022-06-16 16:59:35 -0300

    teste

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,9 +13,9 @@ class MailController extends Controller
 {
     public function teste_mail(){
         
-        $hoje = date('d/m/y');
+        
         /*$users = DB::table('users')->where('id', '!=', '0')->get();*/
-        $refeicaos = Refeicao::where('data_visual', '=', date('d/m/y'))->get();
+        $refeicaos = Refeicao::whereDate('data', '=', date('Y-m-d'))->get();
         $users = User::where('id', '!=', '0')->get();
 
         
