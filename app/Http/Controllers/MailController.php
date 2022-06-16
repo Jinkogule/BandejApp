a8ce31e89772e6fe656e5d9201fc807cfaa7401e
Jinkogule <lucaspimenta@gmail.com>
2022-06-16 15:19:04 -0300

    teste

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,13 +6,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\Refeicaos;
 use App\Mail\NotifyUserAboutSomething;
 
 class MailController extends Controller
 {
     public function teste_mail(){
+        $hoje = date('y-m-d');
         /*$users = DB::table('users')->where('id', '!=', '0')->get();*/
-        $users = User::where('id', '!=', '0')->get();
+        $refeicaos = Refeicaos::where('date', '=', $hoje)->get();
+        $users = User::whereIn('id', $refeicaos)->get();
 
         
         
