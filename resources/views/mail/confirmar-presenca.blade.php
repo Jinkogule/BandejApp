89a7ebff6f3f4a7207a0c6f494b6dcd2c9735c08
Jinkogule <lucaspimenta@gmail.com>
2022-06-22 01:55:21 -0300

    correcao

--- resources/views/mail/confirmar-presenca.blade.php
+++ resources/views/mail/confirmar-presenca.blade.php
@@ -1,3 +1,3 @@
-P<div style="text-transform: lowercase;">rezado(a) {{ $nome }}, você registrou que estará presente no(a) {{ $tipo_refeicao }} amanhã ({{ $dia_da_semana_refeicao }}, dia {{ $data_refeicao }}) no campus {{ $unidade_refeicao }}.</div>
+Prezado(a) {{ $nome }}, você tem um(a) {{ $tipo_refeicao }} amanhã ({{ $dia_da_semana_refeicao }}, dia {{ $data_refeicao }}) no campus {{ $unidade_refeicao }}.
 <br>
 Você confirma sua presença? <a href="https://bandejapp.herokuapp.com/">Acesse o BandejApp</a> para confirmar ou cancelar.
