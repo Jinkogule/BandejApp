0ce546656b04f64d539d30c3f53956ce5bd950d9
Jinkogule <lucaspimenta@gmail.com>
2022-06-22 00:23:32 -0300

    teste

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -15,14 +15,17 @@ class MailController extends Controller
         
         
         /*$users = DB::table('users')->where('id', '!=', '0')->get();*/
-        $refeicaos = Refeicao::whereDate('data', '=', date('Y-m-d'))->get();
+      
 
         $users_ref_hoje = DB::table('users')->join('refeicaos', 'users.id', '=', 'refeicaos.id_usuario')->select('users.id')->whereDate('refeicaos.data', '=', date('Y-m-d'));
         $users = User::whereIn('id', $users_ref_hoje)->get();
 
         foreach($users as $user){
-            $refeicao = Refeicao::where('id_usuario', '=', '1')->whereDate('data', '=', date('Y-m-d'))->get();
-            Mail::to($user)->send(new NotificaConfirmacaoDePresenca($user, $refeicao));
+            $refeicaos = Refeicao::where('id_usuario', '!=', '1')->whereDate('data', '=', date('Y-m-d'))->get();
+            foreach($refeicaos as $refeicao){
+                Mail::to($user)->send(new NotificaConfirmacaoDePresenca($user, $refeicao));
+            }
+            
         }
         
     }
