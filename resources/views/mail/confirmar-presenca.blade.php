6b1c0d5f42e8d5b43ff0afb406ce7435cb0eee4e
Jinkogule <lucaspimenta@gmail.com>
2022-06-22 01:24:35 -0300

    teste

--- resources/views/mail/confirmar-presenca.blade.php
+++ resources/views/mail/confirmar-presenca.blade.php
@@ -1,3 +1,3 @@
 Prezado(a) {{ $nome }}, você tem um(a) {{ $tipo_refeicao }} amanhã ({{ $dia_da_semana_refeicao }}, dia {{ $data_refeicao }}) no campus {{ $unidade_refeicao }}.
 <br>
-Você confirma sua presença?
+Você confirma sua presença? <a href="https://bandejapp.herokuapp.com/">Acesse o BandejApp</a> para confirmar ou cancelar.
