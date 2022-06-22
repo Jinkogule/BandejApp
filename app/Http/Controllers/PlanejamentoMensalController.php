1554d9b1f136d181eafb00e1ff923d80eccdbf52
Jinkogule <lucaspimenta@gmail.com>
2022-06-22 18:34:34 -0300

    teste

--- app/Http/Controllers/PlanejamentoMensalController.php
+++ app/Http/Controllers/PlanejamentoMensalController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Models\Refeicao;
 
 class PlanejamentoMensalController extends Controller
 {
