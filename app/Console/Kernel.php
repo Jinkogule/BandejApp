4a56256b318085a7397ebf7640cc3c8cf786d8bf
Jinkogule <lucaspimenta@gmail.com>
2022-06-27 16:40:41 -0300

    teste

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,23 +20,22 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        /*Notificação de confirmação de presença*/
+        
         $schedule->call(function () {
             $users_ref_hoje = DB::table('users')->join('refeicaos', 'users.id', '=', 'refeicaos.id_usuario')->select('users.id')->whereDate('refeicaos.data', '=', date('Y-m-d', strtotime(' +1 day')));
             $users = User::whereIn('id', $users_ref_hoje)->get();
 
+            /*Notificação de confirmação de presença*/
             foreach($users as $user){
                 $refeicaos = Refeicao::where('id_usuario', '=', $user->id)->whereDate('data', '=', date('Y-m-d', strtotime(' +1 day')))->get();
                 foreach($refeicaos as $refeicao){
                     Mail::to($user->email)->send(new NotificaConfirmacaoDePresenca($user, $refeicao));
                 }
             }
-        })->everyMinute();
 
-        /*Delete das refeições que já se passaram*/
-        $schedule->call(function () {
+            /*Delete das refeições que já se passaram*/
             DB::table('refeicaos')->whereDate('data', '<', date('Y-m-d'))->delete();
-        })->dailyAt('16:15');
+        })->everyMinute();
     }
 
     /**
