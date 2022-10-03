f678adfa4ae561d3ca8612e767f822d0424aa4ab
Jinkogule <lucaspimenta21@gmail.com>
2022-10-03 19:47:41 -0300

    correcao

--- resources/views/layouts-user/dashboard.blade.php
+++ resources/views/layouts-user/dashboard.blade.php
@@ -45,6 +45,11 @@
                     <div class="card-header">
                         <?php
                         $amanha = date('Y-m-d', strtotime(' +1 day'));
+                        $data_banco = $event->data;  
+                        $data_visual2 = date("d/m/y", strtotime($data_banco));
+                        $dia_da_semana_visual = ucfirst($event->dia_da_semana);
+
+                        $tipo_visual = ucfirst($event->tipo);
 
                         
 
@@ -68,13 +73,9 @@
                         <?php
                     
                         }
-                        $data_banco = $event->data;  
-                        $data_visual2 = date("d/m/y", strtotime($data_banco));
-                        $dia_da_semana_visual = ucfirst($event->dia_da_semana);
-
-                        $tipo_visual = ucfirst($event->tipo);
+     
                         ?>
-                        <span class="card-title" style="text-align: center; color: #fff;">{{$dia_da_semana_visual}} - {{$event->$data_visual2}} - {{$tipo_visual}} - {{$event->unidade_bandejao}}</span>
+                        <span class="card-title" style="text-align: center; color: #fff;">{{$dia_da_semana_visual}} - {{$data_visual2}} - {{$tipo_visual}} - {{$event->unidade_bandejao}}</span>
                     </div>
                     
                     <div class="card-body">
