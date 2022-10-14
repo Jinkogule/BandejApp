0a7f0b8e15e191aed5e4ce79024bfe6d215b6390
Jinkogule <lucaspimenta21@gmail.com>
2022-10-13 23:26:21 -0300

    teste

--- resources/views/layouts-user/components-user/navbar2-testes.blade.php
+++ resources/views/layouts-user/components-user/navbar2-testes.blade.php
@@ -1,10 +1,22 @@
 <nav class="navbar navbar2 navbar-expand-md fixed-top">
     <div class="row" style="width: 100%; height: 45px; padding: 0; margin: 0; position: relative;">
-       
-        <div class="col botoes-menu border position-relative">
-            <p class="centraliza" style="color: #fff;"> Próxima etapa</p>
-            <a href="/dashboard" class="stretched-link"></a>
-        </div>   
-         
+        <?php
+        if (DB::table('refeicaos')->select('*')->where('id_usuario', '=', $user_id)->exists() == 1){
+        ?>
+            <div class="col botoes-menu border position-relative" style="background-color: #44832d !important;">
+                <p class="centraliza" style="color: #fff;"> Próxima etapa</p>
+                <a href="/dashboard" class="stretched-link"></a>
+            </div>
+        <?php
+        }
+        else{
+        ?>
+         <div class="col botoes-menu border position-relative">
+            <p disabled class="centraliza" style="color: #fff;"> Próxima etapa</p>
+            <a disabled href="" class="stretched-link"></a>
+        </div>
+        <?php
+        }
+        ?>
     </div>
 </nav>
