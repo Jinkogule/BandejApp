2f80d4c795f8731bc3729d0b7ac193a6614e6eb1
Jinkogule <lucaspimenta21@gmail.com>
2022-10-13 23:41:01 -0300

    ajuste

--- resources/views/layouts-user/components-user/navbar2-testes.blade.php
+++ resources/views/layouts-user/components-user/navbar2-testes.blade.php
@@ -3,7 +3,7 @@
         <?php
         if (DB::table('refeicaos')->select('*')->where('id_usuario', '=', $user_id)->exists() == 1){
         ?>
-            <div class="col botoes-menu border position-relative" style="background-color: #44832d !important;">
+            <div class="col btn-sucesso border position-relative">
                 <p class="centraliza" style="color: #fff;"> Próxima etapa</p>
                 <a href="/dashboard" class="stretched-link"></a>
             </div>
@@ -11,7 +11,7 @@
         }
         else{
         ?>
-         <div class="col botoes-menu border position-relative">
+         <div class="col btn-desativado border position-relative">
             <p disabled class="centraliza" style="color: #fff;"> Próxima etapa</p>
             <a disabled href="" class="stretched-link"></a>
         </div>
