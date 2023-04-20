2240068654c770fe0e6d53399283e296d791376a
Jinkogule <lucaspimenta21@gmail.com>
2023-04-20 14:09:16 -0300

    add prefix

--- resources/views/layouts-user/components-user/navbar2.blade.php
+++ resources/views/layouts-user/components-user/navbar2.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar2 navbar-expand-md fixed-top" style="z-index: -1;">
+<nav class="navbar navbar2 navbar-expand-md fixed-top">
     <div class="row" style="width: 100%; height: 45px; padding: 0; margin: 0; position: relative;">
         <div class="col botoes-menu border position-relative">
             <p class="centraliza" style="color: #fff;">Próximas Refeições</p>
