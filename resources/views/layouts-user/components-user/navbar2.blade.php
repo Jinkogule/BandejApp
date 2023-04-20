195e4c7614247bde38c8420e31ee5d5780ff3fea
Jinkogule <lucaspimenta21@gmail.com>
2023-04-20 14:00:35 -0300

    teste

--- resources/views/layouts-user/components-user/navbar2.blade.php
+++ resources/views/layouts-user/components-user/navbar2.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar2 navbar-expand-md fixed-top">
-    <div class="row" style="width: 100%; height: 45px; padding: 0; margin: 0; position: relative; z-index: -1;">
+    <div class="row" style="width: 100%; height: 45px; padding: 0; margin: 0; position: relative;">
         <div class="col botoes-menu border position-relative">
             <p class="centraliza" style="color: #fff;">Próximas Refeições</p>
             <a href="/dashboard" class="stretched-link"></a>
