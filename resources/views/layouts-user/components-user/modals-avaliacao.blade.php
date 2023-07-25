b2987a4d396bd918a26db08f0e665e611ac5c216
Lucas Pimenta Santana Murta <lucaspimenta@id.uff.br>
2023-07-25 13:21:06 -0300

    retirando estrelas

--- resources/views/layouts-user/components-user/modals-avaliacao.blade.php
+++ resources/views/layouts-user/components-user/modals-avaliacao.blade.php
@@ -38,57 +38,7 @@
                     <input type="hidden" id="id_refeicao" name="id_refeicao" value="{{$event->id}}">
 
                     <label for="avaliacao">Avaliação:</label>
-                    <style>
-
-.rate {
-    float: left;
-    height: 46px;
-    padding: 0 10px;
-}
-.rate:not(:checked) > input {
-    position:absolute;
-    top:-9999px;
-}
-.rate:not(:checked) > label {
-    float:right;
-    width:1em;
-    overflow:hidden;
-    white-space:nowrap;
-    cursor:pointer;
-    font-size:30px;
-    color:#ccc;
-}
-.rate:not(:checked) > label:before {
-    content: '★ ';
-}
-.rate > input:checked ~ label {
-    color: #ffc700;
-}
-.rate:not(:checked) > label:hover,
-.rate:not(:checked) > label:hover ~ label {
-    color: #deb217;
-}
-.rate > input:checked + label:hover,
-.rate > input:checked + label:hover ~ label,
-.rate > input:checked ~ label:hover,
-.rate > input:checked ~ label:hover ~ label,
-.rate > label:hover ~ input:checked ~ label {
-    color: #c59b08;
-}
-</style>
-                    <div class="rate">
-                        <input type="radio" id="star5" name="avaliacao" value="5" />
-                        <label for="star5" title="text">5 stars</label>
-                        <input type="radio" id="star4" name="avaliacao" value="4" />
-                        <label for="star4" title="text">4 stars</label>
-                        <input type="radio" id="star3" name="avaliacao" value="3" />
-                        <label for="star3" title="text">3 stars</label>
-                        <input type="radio" id="star2" name="avaliacao" value="2" />
-                        <label for="star2" title="text">2 stars</label>
-                        <input type="radio" id="star1" name="avaliacao" value="1" />
-                        <label for="star1" title="text">1 star</label>
-                    </div>
-
+                    <input type="text" id="avaliacao" name="avaliacao" pattern="[0-9]+" required>
 
                     <label for="avaliacao_detalhada">Detalhes de sua avaliação:</label>
                     <input type="text" id="avaliacao_detalhada" name="avaliacao_detalhada" pattern="^[a-zA-Z\s]+$">
@@ -97,8 +47,6 @@
                         <button type="submit" class="btn btn-primary btn-confirmar">Enviar</button>
                     </div>
                 </form>
-
-
             </div>
         </div>
     </div>
