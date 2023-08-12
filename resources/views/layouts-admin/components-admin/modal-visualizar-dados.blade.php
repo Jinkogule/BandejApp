91be381136375b5957f0550b5d03d1aa970d7427
Lucas Pimenta Santana Murta <lucaspimenta@id.uff.br>
2023-08-12 09:52:46 -0300

    novo visual tabela de dados

--- resources/views/layouts-admin/components-admin/modal-visualizar-dados.blade.php
+++ resources/views/layouts-admin/components-admin/modal-visualizar-dados.blade.php
@@ -12,21 +12,21 @@
             <div class="modal-body">
             <ul>
             <div class="table-responsive">
-                <table class="table" style="color: #fff;">
+                <table class="table" style="color: #fff; font-size: 14px;">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Gragoatá</th>
-                        <th scope="col">Praia Vermelha</th>
-                        <th scope="col">Reitoria</th>
-                        <th scope="col">Veterinária</th>
+                        <th scope="col">Grag.</th>
+                        <th scope="col">PV</th>
+                        <th scope="col">Reit.</th>
+                        <th scope="col">Vet.</th>
                         <th scope="col">HUAP</th>
                         <th scope="col">Total</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row">Registrados almoço</th>
+                            <th scope="row">Reg. A</th>
                             <td>{{ $registrados[$event->id]['registrados_almoco_gragoata'] }}</td>
                             <td>{{ $registrados[$event->id]['registrados_almoco_pv'] }}</td>
                             <td>{{ $registrados[$event->id]['registrados_almoco_reitoria'] }}</td>
@@ -34,7 +34,7 @@
                             <td>{{ $registrados[$event->id]['registrados_almoco_huap'] }}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Registrados janta</th>
+                            <th scope="row">Reg. J</th>
                             <td>{{ $registrados[$event->id]['registrados_janta_gragoata'] }}</td>
                             <td>{{ $registrados[$event->id]['registrados_janta_pv'] }}</td>
                             <td>{{ $registrados[$event->id]['registrados_janta_reitoria'] }}</td>
@@ -42,7 +42,7 @@
                             <td>{{ $registrados[$event->id]['registrados_janta_huap'] }}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Total registrados</th>
+                            <th scope="row">Reg. T</th>
                             <td>{{ $registrados[$event->id]['registrados_total_gragoata'] }}</td>
                             <td>{{ $registrados[$event->id]['registrados_total_pv'] }}</td>
                             <td>{{ $registrados[$event->id]['registrados_total_reitoria'] }}</td>
@@ -50,7 +50,7 @@
                             <td>{{ $registrados[$event->id]['registrados_total_huap'] }}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Confirmados almoço</th>
+                            <th scope="row">Conf. A</th>
                             <td>{{ $confirmados[$event->id]['confirmados_almoco_gragoata'] }}</td>
                             <td>{{ $confirmados[$event->id]['confirmados_almoco_pv'] }}</td>
                             <td>{{ $confirmados[$event->id]['confirmados_almoco_reitoria'] }}</td>
@@ -58,7 +58,7 @@
                             <td>{{ $confirmados[$event->id]['confirmados_almoco_huap'] }}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Confirmados janta</th>
+                            <th scope="row">Conf. J</th>
                             <td>{{ $confirmados[$event->id]['confirmados_janta_gragoata'] }}</td>
                             <td>{{ $confirmados[$event->id]['confirmados_janta_pv'] }}</td>
                             <td>{{ $confirmados[$event->id]['confirmados_janta_reitoria'] }}</td>
@@ -66,7 +66,7 @@
                             <td>{{ $confirmados[$event->id]['confirmados_janta_huap'] }}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Total confirmados</th>
+                            <th scope="row">Conf. T</th>
                             <td>{{ $confirmados[$event->id]['confirmados_total_gragoata'] }}</td>
                             <td>{{ $confirmados[$event->id]['confirmados_total_pv'] }}</td>
                             <td>{{ $confirmados[$event->id]['confirmados_total_reitoria'] }}</td>
@@ -77,7 +77,7 @@
                         </tr>
                     </tbody>
                     </table>
-</div>
+                </div>
 
             </ul>
             </div>
